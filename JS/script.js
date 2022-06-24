536a342fbc9d5375c9ae2f30dcfe73d0d7093996
Mark0rtiz <106716676+Mark0rtiz@users.noreply.github.com>
2022-06-23 18:04:34 -0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+//console.log("hello World!");
+
+
+let firstName= "mark";
+let lastName= "ortiz"
+let age= 30;
+let passWord= "password";
+let userId= "user id";
+let video= "video url"
+let genre= "video genre";
+let vidLength= "video length";
+let playlist= "user playlist";
+let human= true;
+let videoPreview= "video preview";
+let thumbnail= "thumbnail";
+let comment= "user comment";
+let recentSearches= "recent searches";
+let account= "user account";
+let loc= "user location";
+let settings= "user settings";
+let appearance= "dark";
+let language= "english";
+let channel= "channel";
+let membership= "user membership";
+
+document.write(` ${firstName}, ${lastName}, ${age}, ${passWord}, ${userId}, ${video}, ${genre}, ${vidLength}, ${playlist}, ${human}, ${videoPreview}, ${thumbnail}, ${comment}, ${recentSearches}, ${account}, ${loc}, ${settings}, ${appearance}, ${language}, ${channel}, ${membership}`);
